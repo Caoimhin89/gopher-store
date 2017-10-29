d8dc0416d40b11a5a5074d0fd4eed1b5e3e3b577
Kevin Glick <kevin.n.glick@gmail.com>
2017-10-29 16:14:28 -0400

    add blogpost model

--- models/blogpost.go
+++ models/blogpost.go
@@ -1,1 +1,16 @@
 package main
+
+import "time"
+
+type Blogpost struct {
+	Title string
+	Subtitle string
+	Excerpt string
+	PubDate time.Time
+	Author string
+	Body string
+	CoverPhoto []byte
+	ContentPhotos [][]byte
+}
+
+type Blog []Blogpost
