f82d41f33e21adced4ef25002c5ffe82de406647
Kevin Glick <kevin.n.glick@gmail.com>
2017-10-29 16:04:35 -0400

    add routes

--- main.go
+++ main.go
@@ -12,11 +12,25 @@ import (
 func main() {
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", Index)
-
+	router.HandleFunc("/products", showProducts)
+	router.HandleFunc("/collections", showCollections)
+	router.HandleFunc("/blog", showBlog)
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
 func Index(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+}
+
+func showProducts(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Products, %q", html.EscapeString(r.URL.Path))
+}
+
+func showCollections(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Collections, %q", html.EscapeString(r.URL.Path))
+}
+
+func showBlog(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Blog, %q", html.EscapeString(r.URL.Path))
 }
 
