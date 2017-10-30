208243dd55241f64f68ccbc8ec3813d11ce7d2ca
Kevin Glick <kevin.n.glick@gmail.com>
2017-10-29 21:03:14 -0400

    refactor user routes

--- routes/userRoutes.go
+++ routes/userRoutes.go
@@ -3,10 +3,48 @@ package main
 import "github.com/gorilla/mux"
 
 func setUserRoutes(router *mux.Router) *mux.Router{
-	router.HandleFunc("/users", showUsers)
-	router.HandleFunc("/users", addUser)
-	router.HandleFunc("/users/{id}", showUser)
-	router.HandleFunc("/users/{id}", updateUser)
-	router.HandleFunc("/users/{id}", deleteUser)
+	for _, route := range userRoutes {
+		router.
+			Methods(route.Method).
+			Path(route.Pattern).
+			Name(route.Name).
+			Handler(route.HandlerFunc)
+	}
+
 	return router
+}
+
+var userRoutes = Routes{
+
+	Route{
+		"GetUsers",
+		"GET",
+		"/users",
+		GetUsers,
+	},
+	Route{
+		"GetUser",
+		"GET",
+		"/users/{id}",
+		GetUser,
+	},
+	Route{
+		"AddUser",
+		"POST",
+		"/users",
+		AddUser,
+	},
+	Route{
+		"EditUser",
+		"PUT",
+		"/users/{id}",
+		EditUser,
+	},
+	Route{
+		"DeleteUser",
+		"DELETE",
+		"/users/{id}",
+		DeleteUser,
+	},
+
 }
